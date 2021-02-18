7dfab2c659ff6403468e8477eef355c203ce9809
Claus Kühnel <info@ckuehnel.ch>
2021-02-18 21:40:04 +0100

    Add files via upload
    Pico Explorer w/ BME280 connected

--- Raspberry Pi Pico/Pi_Pico_BME280.py
+++ Raspberry Pi Pico/Pi_Pico_BME280.py
@@ -1,22 +1,57 @@
-#
-# Test BMx280 Breakout Board
-# Cheap boards equipped with BMP280 or BME280 are offered and you can't
-# see which sensor is on board. You can use this program to test it.
-# BMP280 measures temperature and barometric pressure only, no humidity.
-# The sensors can be differentiated using the DevideID
-#
-# 2021-02-08 Claus Kühnel info@ckuehnel.ch
-#
-from machine import I2C, Pin
-from bme280_float_mod import *
-from utime import sleep
+# Read BME280 sensor and display data on pico explorer LCD
+# 2021-02-17 Claus Kuehnel info@ckuehnel.ch
 
-sda = Pin(0)
-scl = Pin(1)
-i2c=I2C(0)
-bme280 = BME280(i2c=i2c)
+from machine import Pin, I2C
+from time import sleep
+import BME280
+#import Piezo
+
+# Pico Explorer boilerplate
+import picoexplorer as display
+width = display.get_width()
+height = display.get_height()
+display_buffer = bytearray(width * height * 2)
+display.init(display_buffer)
+
+sda=Pin(20)         # Pico Explorer
+scl=Pin(21)
+i2c=I2C(0,sda=sda, scl=scl, freq=400000)
+bme = BME280.BME280(i2c=i2c)
+
+def clear():                        # this function clears Pico Explorer's screen to black
+    display.set_pen(0,0,0)
+    display.clear()
+    display.update()
+    
+clear()
+display.set_pen(0, 255, 0)
+display.text('Weather Station', 0, 100, width, 3)
+display.update()
+# Piezo.playsong(Piezo.song)
+sleep(2)
+    
 
 while True:
-    if bme280.isBME280: print(bme280.values)
-    else: print(bme280.BMP280_values) # no humidity when BMP280
-    sleep(1)
+  temp = bme.read_temperature()/100
+  hum = bme.read_humidity()/1000
+  pres = bme.read_pressure()/25600
+  
+  print('Temperature: {:.1f} °C'.format(temp))
+  print('Humidity:    {:.1f} %rH'.format(hum))
+  print('Pressure:    {:.0f} hPa'.format(pres))
+  print("")
+  
+  # writes the reading as text
+  clear()
+  display.set_pen(0, 255, 0)
+  display.text('Temp: {:.1f} *C'.format(temp), 10, 60, width, 3)
+  display.text('Humi: {:.1f} %rH'.format(hum), 10, 100, width, 3)
+  display.text('Pres: {:.0f} hPa'.format(pres), 10,140, width, 3)
+    
+  # time to update the display
+  display.update()
+  
+
+  sleep(5)
+    
+
