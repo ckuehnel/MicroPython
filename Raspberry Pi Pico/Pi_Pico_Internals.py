f55fc7d9f7d666e039c71567799379a44c535bd3
Claus Kühnel <info@ckuehnel.ch>
2021-03-10 12:58:02 +0100

    Update Pi_Pico_Internals.py

--- Raspberry Pi Pico/Pi_Pico_Internals.py
+++ Raspberry Pi Pico/Pi_Pico_Internals.py
@@ -1,39 +1,20 @@
-# Pi_Pico_Internals.py
-# Raspberry Pi Pico connected w/ Pimoroni Pico Display
-# This sample program shows some information 
+# RaspberryPiPico_internals.py
+# This sample program shows some information
 # and uses some on-board resources to illustrate their query
-# (c) 2021-02-04 Claus Kuehnel (info@ckuehnel.ch)
-	
-import machine, time, sys, uos
-import picodisplay as display
+# (c) 2021-03-09 Claus Kuehnel (info@ckuehnel.ch)
 
 from machine import Timer
 from machine import Pin
 
-led = Pin(25, Pin.OUT)   # external LED on Pi Pico
-t   = Timer()
+import sys, uos, ubinascii
 
-sensor_temp = machine.ADC(4)
-conversion_factor = 3.3 / (65536)
+def tick(timer):
+    global led
+    led.toggle()
 
-def blink(Timer):
-	led(1)
-	time.sleep_ms(20)      # LED on for 20 milliseconds
-	led(0)
-
-def initDisplay():
-    width = display.get_width()
-    height = display.get_height()
-
-    display_buffer = bytearray(width * height * 2)  # 2-bytes per pixel (RGB565)
-    display.init(display_buffer)
-    display.set_backlight(1)
-
-    display.set_pen(0, 0, 0)    # black
-    display.clear()
-    display.set_pen(100, 100, 100) # white
-
-t.init(period=1000, mode=Timer.PERIODIC, callback = blink)
+led = Pin(25, Pin.OUT) # green LED on Pi Pico
+timer = Timer()
+timer.init(freq=2, mode=Timer.PERIODIC, callback=tick)
 
 print('\nThis program will show some information')
 print('and the usage of some on-board ressources of Raspberry Pi Pico Board\n')
@@ -44,39 +25,6 @@ print('Platform is {}'.format(sys.platform))
 print('Micropython version is {}'.format(sys.version))
 print('CPU frequency is {:3.0f} MHz'.format(machine.freq()/1e6))
 
-reading = sensor_temp.read_u16() * conversion_factor
-    
-# The temperature sensor measures the Vbe voltage of a biased bipolar diode, connected to the fifth ADC channel
-# Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree. 
-intTemp = 27 - (reading - 0.706)/0.001721
-print('On-Chip temperature is {:3.1f} °C'.format(intTemp))
-
-initDisplay()
-width = display.get_width()
-txt = '{}'.format(uos.uname().machine)
-display.text(txt,10,10, width, 2)
-txt = 'Firmware v.{:}'.format(uos.uname().version)
-display.text(txt,10, 40, width, 2)
-txt = 'Clock is {:3.0f} MHz'.format(machine.freq()/1e6)
-display.text(txt,10, 85, width, 2)
-txt = 'Temp is {:3.1f} *C'.format(intTemp)
-display.text(txt,10, 100, width, 2)
-display.update()
-
-print('Press the X button for fast blinking & Y botton for slow blinking')
-print('Ctrl-C stopps the running program')
-
-global state
-state = 0
-
-while True:
-    if display.is_pressed(display.BUTTON_X) == 1 and state == 0:
-        print('X-Button pressed - fast blinking')
-        state = 1
-        t.init(period=100, mode=Timer.PERIODIC, callback = blink)
-    if display.is_pressed(display.BUTTON_Y) == 1 and state == 1:
-        print('Y-Button pressed - slow blinking')
-        state = 0
-        t.init(period=1000, mode=Timer.PERIODIC, callback = blink)
-        
-        
+UID = ubinascii.hexlify(machine.unique_id()).decode('utf-8')
+print('Length of UID is {} bytes'.format(int(len(UID)/2)))
+print('UUID is {}\n'.format(UID))
